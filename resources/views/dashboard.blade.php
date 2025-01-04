5f0332d6a6d5ede542490e461c06b736e7a47ae1
jasdmadriaga <jasdmadriaga@gmail.com>
2025-01-04 21:42:28 +0800

    This is the final FINAL project, ipasa mo kami Sir

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <h1 style="font-family: 'Arial Black', Monaco, monospace;
+font-size: 25px;
+letter-spacing: 2px;
+word-spacing: 2px;
+color: #000000;
+font-weight: normal;
+text-decoration: none;
+font-style: normal;
+font-variant: normal;
+text-transform: none;"> The UP SLIS Library Database </h1>
+            <p> Abris, Beltran, Lopez, Madriaga, Pangcoga</p>
+            <p> The GOAT: Asst. Prof. Janny Surmieda </p>
             </div>
         </div>
     </div>
